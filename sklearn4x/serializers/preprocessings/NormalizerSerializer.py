b714ecc329666d060d504c3a3d1f15ba4b90b575
Mohammad Ali Yektaie <ma_yektaie@outlook.com>
2022-06-20 09:00:08 -0700

    Fixed the inner class error in Normalizer and MinMaxScaler.

--- sklearn4x/serializers/preprocessings/NormalizerSerializer.py
+++ sklearn4x/serializers/preprocessings/NormalizerSerializer.py
@@ -1,5 +1,9 @@
+# ==================================================================
+# Serialize Normalizer
+#
+# Scaffolded from: https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.Normalizer.html#sklearn.preprocessing.Normalizer
+# ==================================================================
 from sklearn4x.core.BaseSerializer import BaseSerializer
-
 
 class NormalizerSerializer(BaseSerializer):
     def identifier(self):
@@ -8,29 +12,9 @@ class NormalizerSerializer(BaseSerializer):
     def get_fields_to_be_serialized(self, model, version):
         fields = []
 
-        self.add_field(fields, "copy", model.copy)
-        self.add_field(fields, "n_features_in_", model.n_features_in_)
-        self.add_field(fields, "norm", model.norm)
+        self.add_field(fields, "norm", self.get_value_or_none(model, "norm"))
+
+        self.add_n_features(fields, model)
+        self.add_feature_names(fields, model)
 
         return fields
-
-    # ==================================================================
-    # Serialize Normalizer
-    #
-    # Scaffolded from: https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.Normalizer.html#sklearn.preprocessing.Normalizer
-    # ==================================================================
-    from sklearn4x.core.BaseSerializer import BaseSerializer
-
-    class NormalizerSerializer(BaseSerializer):
-        def identifier(self):
-            return 'pp_normalizer'
-
-        def get_fields_to_be_serialized(self, model, version):
-            fields = []
-
-            self.add_field(fields, "norm", self.get_value_or_none(model, "norm"))
-
-            self.add_n_features(fields, model)
-            self.add_feature_names(fields, model)
-
-            return fields
