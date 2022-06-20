6a9de42802b8f56c7c978c851f45af01dfe575f8
Mohammad Ali Yektaie <ma_yektaie@outlook.com>
2022-06-20 05:22:18 -0700

    Fixed the ReobustScaler.

--- tools/train_classifier_for_debugging.py
+++ tools/train_classifier_for_debugging.py
@@ -1,8 +1,8 @@
 import sklearn
 from sklearn import datasets
-from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.preprocessing import RobustScaler
 from sklearn4x.sklearn4x import save_scikit_learn_model
-
+import sklearn.datasets as ds
 import scipy.sparse as sp
 import numpy as np
 import warnings
@@ -11,9 +11,9 @@ warnings.filterwarnings("ignore")
 
 # print('scikit-learn version: ' + sklearn.__version__)
 
-X = [{'sci-fi', 'thriller'}, {'comedy'}]
+X = ds.load_iris().data[0:15]
 
-preprocessing = MultiLabelBinarizer(classes=['aaa', 'thriller', 'comedy', 'sci-fi'])
+preprocessing = RobustScaler()
 preprocessing.fit(X)
 
 transformed = preprocessing.transform(X)
@@ -22,9 +22,9 @@ if isinstance(transformed, sp.csr_matrix):
 
 test_data = {
     "template_version": "preprocessings_v1",
-    "configurations": {'config_name': 'with string labels and specified classes', 'additional_import': '', 'class_argument': "classes=['aaa', 'thriller', 'comedy', 'sci-fi']", 'custom_assertions': [], 'custom_transform_input': 'X', 'input': "X = [{'sci-fi', 'thriller'}, {'comedy'}]"},
+    "configurations": {'config_name': 'simple case without customization', 'additional_import': 'import sklearn.datasets as ds', 'class_argument': '', 'custom_assertions': [], 'custom_transform_input': 'X', 'input': 'X = ds.load_iris().data[0:15]'},
     "raw": X,
     "transformed": transformed,
 }
 
-save_scikit_learn_model({'preprocessing_to_test': preprocessing}, "/Users/yektaie/Documents/Generated Unit Tests/binaries/1.0.2/3.9/multi_label_binarizer_with_string_labels_and_specified_classes.skx", test_data)
+save_scikit_learn_model({'preprocessing_to_test': preprocessing}, "/Users/yektaie/Documents/Generated Unit Tests/binaries/1.0.1/3.8/robust_scaler_simple_case_without_customization.skx", test_data)
